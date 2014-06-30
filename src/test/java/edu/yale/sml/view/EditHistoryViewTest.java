997f00921e18b51f01f2101898964ccce88adc0b
osmandin <osmandin@gmail.com>
2014-06-30 09:43:15 -0400

    add edithistoryview test

--- src/test/java/edu/yale/sml/view/EditHistoryViewTest.java
+++ src/test/java/edu/yale/sml/view/EditHistoryViewTest.java
@@ -1,38 +1,39 @@
 package edu.yale.sml.view;
 
+import edu.yale.sml.model.History;
 import org.junit.Test;
 
-/**
- *
- */
-public class EditHistoryViewTest {
-    @Test
-    public void testInitialize() throws Exception {
 
-    }
+public class EditHistoryViewTest {
 
     @Test
     public void testGetID() throws Exception {
+        EditHistoryView editHistoryView = new EditHistoryView();
+        editHistoryView.setID(3);
+        assert (editHistoryView.getID() == 3);
 
     }
 
     @Test
     public void testSetID() throws Exception {
+        EditHistoryView editHistoryView = new EditHistoryView();
+        editHistoryView.setID(3);
+        assert (editHistoryView.getID() == 3);
 
     }
 
     @Test
     public void testGetHistoryCatalog() throws Exception {
-
+        EditHistoryView editHistoryView = new EditHistoryView();
+        editHistoryView.setHistoryCatalog(new History());
+        assert (editHistoryView.getHistoryCatalog() != null);
     }
 
     @Test
     public void testSetHistoryCatalog() throws Exception {
-
+        EditHistoryView editHistoryView = new EditHistoryView();
+        editHistoryView.setHistoryCatalog(new History());
+        assert (editHistoryView.getHistoryCatalog() != null);
     }
 
-    @Test
-    public void testSave() throws Exception {
-
-    }
 }
