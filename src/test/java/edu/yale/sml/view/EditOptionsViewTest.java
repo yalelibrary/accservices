bf9fb79dc20fc9bb94ec6f732457aa4b03748b57
osmandin <osmandin@gmail.com>
2014-06-30 15:30:35 -0400

    add edit options view test

--- src/test/java/edu/yale/sml/view/EditOptionsViewTest.java
+++ src/test/java/edu/yale/sml/view/EditOptionsViewTest.java
@@ -2,22 +2,20 @@ package edu.yale.sml.view;
 
 import org.junit.Test;
 
-/**
- *
- */
-public class EditOptionsViewTest {
-    @Test
-    public void testJump() throws Exception {
 
-    }
+public class EditOptionsViewTest {
 
     @Test
     public void testGetRedirect_id() throws Exception {
-
+        EditOptionsView editOptionsView = new EditOptionsView();
+        editOptionsView.setRedirect_id("/shelfscan/index.xhtml");
+        assert (editOptionsView.getRedirect_id().contains("/shelfscan/index.xhtml"));
     }
 
     @Test
     public void testSetRedirect_id() throws Exception {
-
+        EditOptionsView editOptionsView = new EditOptionsView();
+        editOptionsView.setRedirect_id("/shelfscan/index.xhtml");
+        assert (editOptionsView.getRedirect_id().contains("/shelfscan/index.xhtml"));
     }
 }
